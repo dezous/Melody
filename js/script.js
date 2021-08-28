73c3560e74eda3fddeff88a11fb1e323b7af6c04
dezous <vlad.karlyuk5@gmail.com>
2021-08-28 12:11:32 +0300

    Добавлено модальное окно
    Добавил модальное окно с интерактивным выбором квартиры, открывается при нажатии на этаж или кнопку "Смотреть  квартиры на этаже", при наведении на картинке на какую либо из квартир, справа в списке подсвечивается текущая квартира, так и наоборот.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ $(document).ready(function () {
     var currentFloor = 2;
     var counterUp = $('.counter-btn--up'); // кнопка увеличения этажа
     var counterDown = $('.counter-btn--down'); // кнопка уменьшения этажа
-    var floorPath =  $('.home-img path') // наши этажи SVG
+    var floorPath = $('.home-img path') // наши этажи SVG
+    var apartmentsPath = $('.flats path') // наши квартиры SVG
+    var flats = $('.flats__link'); // наши квартиры
+    var viewFlatsButton = $('.view-flats'); // кнопка "Смотреть  квартиры на этаже"
+    var modal = $('.modal'); // модальное окно
+    var modalClose = $('.modal__close'); // кнопка для закрытия модального окна
+
+    floorPath.on('click', toggleModal) // при клике на этаж - открыть окно
+
+    modalClose.on('click', toggleModal) // при клике на кнопку "закрыть" - убрать окно
+
+    viewFlatsButton.on('click', toggleModal) // при клике на кнопку "Смотреть  квартиры на этаже" - открыть окно окно
 
     // При наведении мышкой на этаж 
     floorPath.on('mouseover', function () {
@@ -14,10 +25,12 @@ $(document).ready(function () {
 
     // При нажатии на стрелку вверх
     counterUp.on('click', function () {
-        if (currentFloor < 18 ) {
+        if (currentFloor < 18) {
             currentFloor++; // увеличиваем этаж на 1
-            usCurrentFloor = currentFloor.toLocaleString('en-Us', { minimumIntegerDigits: 2, 
-                useGrouping: false }); //  форматируем переменную с этажем, чтобы было 01, а не 1
+            usCurrentFloor = currentFloor.toLocaleString('en-Us', {
+                minimumIntegerDigits: 2,
+                useGrouping: false
+            }); //  форматируем переменную с этажем, чтобы было 01, а не 1
             $('.counter').text(usCurrentFloor); // изменяем текущий счетчик
             floorPath.removeClass('current-floor'); // убираем активный класс у этажей
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); // добавляем активный класс текущему этажу
@@ -25,13 +38,36 @@ $(document).ready(function () {
     })
     // При нажатии на стрелку вниз
     counterDown.on('click', function () {
-        if (currentFloor > 2 ) {
+        if (currentFloor > 2) {
             currentFloor--; // уменьшаем этаж на 1
-            usCurrentFloor = currentFloor.toLocaleString('en-Us', { minimumIntegerDigits: 2, 
-                useGrouping: false }); //  форматируем переменную с этажем, чтобы было 01, а не 1
+            usCurrentFloor = currentFloor.toLocaleString('en-Us', {
+                minimumIntegerDigits: 2,
+                useGrouping: false
+            }); //  форматируем переменную с этажем, чтобы было 01, а не 1
             $('.counter').text(usCurrentFloor); // изменяем текущий счетчик
             floorPath.removeClass('current-floor'); // убираем активный класс у этажей
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor'); // добавляем активный класс текущему этажу
         }
     })
+
+    // При наведении на квартиру на картинке
+    apartmentsPath.on('mouseover', function () {
+        apartmentsPath.removeClass('active'); // убираем активный класс у всех квартир на картинке
+        flats.removeClass('active'); // убираем активный класс у всех квартир в списке
+        var currentFlat = $(this).attr('data-apartments'); // берем текущий номер квартиры
+        $(`[data-flat='${currentFlat}']`).addClass('active'); // ищем такой же в списке и даем активный класс
+    });
+
+    // При наведении на квартиру в списке
+    flats.on('mouseover', function () {
+        apartmentsPath.removeClass('active'); // убираем активный класс у всех квартир в списке
+        flats.removeClass('active');// убираем активный класс у всех квартир на картинке
+        var currentFlat = $(this).attr('data-flat'); // берем текущий номер квартиры
+        $(`[data-apartments='${currentFlat}']`).addClass('active'); // ищем такой же на картинке и даем активный класс
+    });
+
+    function toggleModal() {
+        // функция открыть-закрыть окно
+        modal.toggleClass('open');
+    }
 });
